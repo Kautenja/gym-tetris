d4b853a61fed2dfb6f154a5b2e573a0805f70f80
Kautenja <ckfour@icloud.com>
2018-05-24 15:12:51 -0500

    env registration

---
+++ gym_tetris/__init__.py
@@ -0,0 +1,39 @@
+"""Registration code of Gym environments in this package."""
+import math
+import gym
+from .tetris_env import TetrisEnv
+
+
+gym.envs.registration.register(
+    id='Tetris-v0',
+    entry_point='gym_tetris:TetrisEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 4,
+    },
+    nondeterministic=True,
+)
+
+
+gym.envs.registration.register(
+    id='Tetris-v0',
+    entry_point='gym_tetris:TetrisEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 1,
+    },
+    nondeterministic=True,
+)
+
+
+def make(environment: str) -> gym.Env:
+    """Make the environment and return it. same as `gym.make`."""
+    return gym.make(environment)
+
+
+# define the outward facing API of this module (none, gym provides the API)
+__all__ = [make.__name__]
