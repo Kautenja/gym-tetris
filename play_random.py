44073abdecce13f1d95380a254b4448fd3dbd69d
Kautenja <ckfour@icloud.com>
2018-05-24 23:30:11 -0500

    update random

--- play_random.py
+++ play_random.py
@@ -1,9 +1,13 @@
 """A simple script for debugging the Super Mario Bros. Lua code."""
 from tqdm import tqdm
+import gym
 import gym_tetris
 
 
 env = gym_tetris.make('Tetris-v0')
+env = gym_tetris.wrap(env)
+# env = gym.wrappers.Monitor(env, './monitor', force=True)
+print(env)
 
 
 try:
