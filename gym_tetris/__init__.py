eb4661752da05b6630c7c3fb8d71632c70724402
Kautenja <ckfour@icloud.com>
2018-05-24 22:57:48 -0500

    update init

--- gym_tetris/__init__.py
+++ gym_tetris/__init__.py
@@ -2,6 +2,14 @@
 import math
 import gym
 from .tetris_env import TetrisEnv
+from .wrappers import (
+    ClipRewardEnv,
+    DownsampleEnv,
+    FrameStackEnv,
+    MaxFrameskipEnv,
+    PenalizeDeathEnv,
+    RewardCacheEnv,
+)
 
 
 gym.envs.registration.register(
@@ -19,8 +27,54 @@ def make(environment: str) -> gym.Env:
     return gym.make(environment)
 
 
+def make_wrapped(
+    game_name: str,
+    image_size: tuple=(84, 84),
+    skip_frames: int=4,
+    death_penalty: int=-1,
+    clip_rewards: bool=True,
+    agent_history_length: int=4
+):
+    """
+    Build and return a configured a wrapped environment.
+
+    Args:
+        game_name: the name of the game to make
+        image_size: the size to down-sample images to
+        skip_frames: the number of frames to hold each action for
+        death_penatly: the penalty for losing a life in a game
+        clip_rewards: whether to clip rewards in {-1, 0, +1}
+        agent_history_length: the size of the frame buffer for the agent
+
+    Returns:
+        a gym environment configured for this experiment
+
+    """
+    # make the initial environment
+    env = gym.make('{}-v0'.format(game_name))
+    # wrap the environment with a reward cacher
+    env = RewardCacheEnv(env)
+    # apply the frame skip feature if enabled
+    if skip_frames is not None:
+        env = MaxFrameskipEnv(env, skip=skip_frames)
+    # apply a down-sampler for the given game
+    env = DownsampleEnv(env, image_size, **DownsampleEnv.metadata[game_name])
+    # apply the death penalty feature if enabled
+    if death_penalty is not None:
+        env = PenalizeDeathEnv(env, penalty=death_penalty)
+    # clip the rewards in {-1, 0, +1} if the feature is enabled
+    if clip_rewards:
+        env = ClipRewardEnv(env)
+    # apply the back history of frames if the feature is enabled
+    if agent_history_length is not None:
+        env = FrameStackEnv(env, agent_history_length)
+
+    return env
+
+
 # define the outward facing API of this module (none, gym provides the API)
 __all__ = [
     TetrisEnv.__name__,
     make.__name__,
+    make_wrapped.__name__,
 ]
