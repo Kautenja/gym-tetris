24cc9c9e3a27aaab54601d9ce73570ec29651499
Kautenja <kautencreations@gmail.com>
2018-06-14 18:38:01 -0500

    update FPS

--- gym_tetris/tetris_env.py
+++ gym_tetris/tetris_env.py
@@ -10,10 +10,9 @@ class TetrisEnv(gym.Env, gym.utils.EzPickle):
     """An environment for playing Tetris in OpenAI Gym."""
 
     # meta-data about the environment for OpenAI Gym utilities (like Monitor)
-    # TODO: update frames_per_second with correct value
     metadata = {
         'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second': 25,
+        'video.frames_per_second': 30,
     }
 
     def __init__(self, max_steps: int, random_state: int = None) -> None:
