a0cab90f9009eb6bcdc45fb7b635fbd456d76c8f
Christian Kauten <Kautenja@users.noreply.github.com>
2019-05-24 15:12:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as README_md:
 
 setup(
     name='gym_tetris',
-    version='2.0.2',
+    version='2.1.0',
     description='Tetris (NES) for OpenAI Gym',
     long_description=README,
     long_description_content_type='text/markdown',
