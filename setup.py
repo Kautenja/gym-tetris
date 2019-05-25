18a5a231513f335a88c216ebd2b56f1c2f448cda
Kautenja <ckfour@icloud.com>
2019-05-24 20:41:46 -0500

    update project metadata
    bump version
    change supported python versions
    set to production / stable

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as README_md:
 
 setup(
     name='gym_tetris',
-    version='2.2.0',
+    version='2.2.1',
     description='Tetris (NES) for OpenAI Gym',
     long_description=README,
     long_description_content_type='text/markdown',
@@ -21,15 +21,15 @@ setup(
     ]),
     classifiers=[
         'License :: Free For Educational Use',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Games/Entertainment :: Side-Scrolling/Arcade Games',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
