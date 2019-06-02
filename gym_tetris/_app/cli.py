faed15d01091730bb8c5c860878869a80689afd0
Christian Kauten <Kautenja@users.noreply.github.com>
2019-06-02 12:40:19 -0500

    Update cli.py
    customize the action space

--- gym_tetris/_app/cli.py
+++ gym_tetris/_app/cli.py
@@ -1,8 +1,17 @@
 """Tetris for OpenAI Gym."""
 import argparse
 import gym
+from nes_py.wrappers import JoypadSpace
 from nes_py.app.play_human import play_human
 from nes_py.app.play_random import play_random
+from ..actions import MOVEMENT, SIMPLE_MOVEMENT
+
+
+# a key mapping of action spaces to wrap with
+_ACTION_SPACES = {
+    'simple': SIMPLE_MOVEMENT,
+    'standard': MOVEMENT,
+}
 
 
 def _get_args():
@@ -20,6 +29,12 @@ def _get_args():
         choices=['human', 'random'],
         help='The execution mode for the environment.'
     )
+    parser.add_argument('--actionspace', '-a',
+        type=str,
+        default='nes',
+        choices=['nes', 'standard', 'simple'],
+        help='the action space wrapper to use'
+    )
     parser.add_argument('--steps', '-s',
         type=int,
         default=500,
@@ -34,6 +49,12 @@ def main():
     args = _get_args()
     # build the environment with the given ID
     env = gym.make(args.env)
+    # wrap the environment with an action space if specified
+    if args.actionspace != 'nes':
+        # unwrap the actions list by key
+        actions = _ACTION_SPACES[args.actionspace]
+        # wrap the environment with the new action space
+        env = JoypadSpace(env, actions)
     # play the environment with the given mode
     if args.mode == 'human':
         play_human(env)
