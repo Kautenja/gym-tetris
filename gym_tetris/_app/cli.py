ee963242d30ed154ce60b69ac9bc75e785ccd82b
Christian Kauten <Kautenja@users.noreply.github.com>
2019-06-02 12:36:51 -0500

    Update cli.py
    update to nes-py 8.0

--- gym_tetris/_app/cli.py
+++ gym_tetris/_app/cli.py
@@ -1,7 +1,6 @@
 """Tetris for OpenAI Gym."""
 import argparse
 import gym
-from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 from nes_py.app.play_human import play_human
 from nes_py.app.play_random import play_random
 
@@ -9,7 +8,7 @@ from nes_py.app.play_random import play_random
 def _get_args():
     """Parse command line arguments and return them."""
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--environment', '-e',
+    parser.add_argument('--env', '-e',
         type=str,
         default='Tetris-v0',
         choices=['Tetris-v0', 'Tetris-v1'],
@@ -26,7 +25,6 @@ def _get_args():
         default=500,
         help='The number of random steps to take.',
     )
-
     return parser.parse_args()
 
 
@@ -35,7 +33,7 @@ def main():
     # parse arguments from the command line (argparse validates arguments)
     args = _get_args()
     # build the environment with the given ID
-    env = gym.make(args.environment)
+    env = gym.make(args.env)
     # play the environment with the given mode
     if args.mode == 'human':
         play_human(env)
