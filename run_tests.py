b4895c438f0a3eaafb0d76dbb2fc88420c2e677d
Christian Kauten <Kautenja@users.noreply.github.com>
2020-07-29 15:04:56 -0500

    Delete run_tests.py

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-"""A script to run the test cases in the package."""
-import os
-import unittest
-
-
-if __name__ == '__main__':
-    package = os.path.dirname(os.path.abspath(__file__))
-    suite = unittest.defaultTestLoader.discover(package)
-    unittest.runner.TextTestRunner().run(suite)
