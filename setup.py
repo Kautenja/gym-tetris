501f25613866b9cf7caf6ecfe705aa9b045cdd86
Kautenja <ckfour@icloud.com>
2022-09-09 13:33:48 -0600

    bump version to 3.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as README_md:
 
 setup(
     name='gym_tetris',
-    version='3.0.3',
+    version='3.0.4',
     description='Tetris (NES) for OpenAI Gym',
     long_description=README,
     long_description_content_type='text/markdown',
